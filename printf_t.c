2fa068bfa386bf31eea30aad937a0b336c47d230
ignisious254 <ignisioussmile@gmail.com>
2023-08-21 15:01:29 -0700

    Removed a char buffer in printf_t.c

--- printf_t.c
+++ printf_t.c
@@ -10,7 +10,6 @@
 int _printf(const char *format, ...)
 {
 	int count = 0;
-	char buffer[100];
 	va_list args;
 
 	/* if the format is NULL, exit with error message*/
@@ -47,11 +46,10 @@ int _printf(const char *format, ...)
 			else if (*format == 's')/*if format is %s print string*/
 			{
 				  char *str_arg = va_arg(args, char *);
-                total_written += snprintf(buffer + total_written, sizeof(buffer) - total_written, "%s", str_arg);				/*char *str = va_arg(args, char*);
-				*	int str_len = strlen(str);
+				  	int str_len = strlen(str);
 
-				*write(1, str, str_len);
-				*count += str_len;*/
+				write(1, str, str_len);
+				count += str_len;
 			}
 			else if (*format == 'd' || *format == 'i')
 			{
