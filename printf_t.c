aaa106c077ffe422b33ca780f82d9bbb04bd9d8c
ignisious254 <ignisioussmile@gmail.com>
2023-08-22 02:18:13 -0700

    Betty compliance

--- printf_t.c
+++ printf_t.c
@@ -1,31 +1,23 @@
 #include "main.h"
-
 /**
  * _printf - produces output according to a format
  * @format:input
- *
  * Return: printed bytes
  */
-
 int _printf(const char *format, ...)
 {
 	int count = 0;
 	va_list args;
-
-	/* if the format is NULL, exit with error message*/
+       /*if format is NULL exit with an error message*/
 	if (format == NULL)
 		return (-1);
 	va_start(args, format);
-
-	/*if format is present continue to the loop*/
-	while (*format)
+	while (*format)/*format is present continue to the loop*/
 	{
-		/*if format is not % you print value*/
-		if (*format != '%')
+		if (*format != '%')/*If format is not % print the value*/
 		{
 			write(1, format, 1);
-			count++;
-		}
+			count++; }
 		else /*if format is % then continue to loop*/
 		{
 			format++;
@@ -34,39 +26,20 @@ int _printf(const char *format, ...)
 			if (*format == '%') /*if format is %% print %*/
 			{
 				write(1, format, 1);
-				count++;
-			}
+				count++; }
 			else if (*format == 'c')/*if format is %c print characters*/
 			{
 				char c = va_arg(args, int);
 
 				write(1, &c, 1);
-				count++;
-			}
+				count++; }
 			else if (*format == 's')/*if format is %s print string*/
 			{
-				  char *str = va_arg(args, char *);
-				  int str_len = strlen(str);
+				char *str = va_arg(args, char *);
+				int str_len = strlen(str);
 
 				write(1, str, str_len);
-				count += str_len;
-			}
-			else if (*format == 'd' || *format == 'i')
-			{
-				int d = va_arg(args, int);
-				/* Use a temporary buffer to convert the int to string*/
-				char buffer[20];
-				int int_len = snprintf(buffer, sizeof(buffer), "%d", d);
-
-				write(1, buffer, int_len);
-				count += int_len;
-			}
-			/*else 
-			*	write(1, format, 1);
-			*	count++;*/
-		}
-		format++;
-	}
+				count += str_len; }
+		} format++;
 	va_end(args);
-	return (count);
-}
+	return (count); }
